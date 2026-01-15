cc6a6505ae81ef7a88526d226e46f982b498beda
Kateryna Troyan <ket.tro@gmail.com>
2026-01-15 20:46:18 +0200

    Add Calculator class with read, sum, and mul methodsЄ

---
+++ js/task7.js
@@ -0,0 +1,27 @@
+//Створіть функцію-конструктор Calculator,
+// який створює об’єкти з трьома методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+// class Calculator {
+//   constructor() {
+//     this.a = 0;
+//     this.b = 0;
+//   }
+//   read() {
+//     this.a = Number(prompt("Введіть перше число:", 0));
+//     this.b = Number(prompt("Введіть друге число:", 0));
+//   }
+//   sum() {
+//     return this.a + this.b;
+//   }
+//   mul() {
+//     return this.a * this.b;
+//   }
+// }
+
+// const calc = new Calculator();
+// calc.read();
+// alert(calc.sum());
+// alert(calc.mul());
